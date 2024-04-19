90740c41e121772b6501b11812e25583d0e4eab8
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2024-04-20 03:04:21 +0530

    added reveal to EventsCarousel

--- src/app/components/EventsCarousel.tsx
+++ src/app/components/EventsCarousel.tsx
@@ -8,6 +8,7 @@ import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import Reveal from './Reveal';
 
 interface EventsCarouselProps{
   images: string[]
@@ -16,30 +17,32 @@ interface EventsCarouselProps{
 const EventsCarousel = ({ images } : EventsCarouselProps) => {
 
   return (
-    <div className="flex justify-start items-center w-screen overflow-hidden">
-      <Swiper
-        cssMode={true}
-        navigation={true}
-        pagination={true}
-        loop={true}
-        mousewheel={true}
-        keyboard={true}
-        modules={[Navigation, Mousewheel, Keyboard]}
-        className="mySwiper" style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
-      >
-        {
-          images.map((imageUrl, index) => (
-            <SwiperSlide key={index} className='flex justify-center items-center w-full overflow-hidden'>
-              <div className='flex justify-center items-center gap-10' style={{ height: '400px'}}>
-                <div className='flex justify-center items-center content-center'>
-                  <Image src={imageUrl} alt={`${index}`} height={300} width={300}></Image>
+    <Reveal>
+      <div className="flex justify-start items-center w-screen overflow-hidden">
+        <Swiper
+          cssMode={true}
+          navigation={true}
+          pagination={true}
+          loop={true}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Navigation, Mousewheel, Keyboard]}
+          className="mySwiper" style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+        >
+          {
+            images.map((imageUrl, index) => (
+              <SwiperSlide key={index} className='flex justify-center items-center w-full overflow-hidden'>
+                <div className='flex justify-center items-center gap-10' style={{ height: '400px'}}>
+                  <div className='flex justify-center items-center content-center'>
+                    <Image src={imageUrl} alt={`${index}`} height={300} width={300}></Image>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))
-        }
-      </Swiper>
-    </div>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
+    </Reveal>
   );
 };
 
